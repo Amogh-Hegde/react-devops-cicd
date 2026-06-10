e8cf9482d2a02327f5f3744242952b69d47a2091
Amogh Hegde <amoghhegde@Amoghs-MacBook-Air.local>
2026-06-10 09:38:36 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || 'https://react-devops-cicd.onrender.com'
-
+const chetan = 2
 function App() {
   const [formData, setFormData] = useState({
     name: '',
